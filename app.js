5e59e3e92a0ef1c776b8918bb4ca1c86313e2f0f
Amber Murray <amber.applebee@gmail.com>
2017-05-16 18:45:58 -0700

    added collections and paintings files to models/routes/collections folders

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 
 const artists = require('./routes/artists')
+const paintings = require('./routes/paintings')
+const collections = require('./routes/collections')
 
 //bodyParser goes here so that the routes can use it
 app.use(bodyParser.json())
 
 app.use('/artists', artists)
-
+app.use('/paintings', paintings)
+app.use('/collections', collections)
 
 
 
